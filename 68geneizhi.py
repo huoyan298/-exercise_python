022d0e318a6a5ddb62069bbdbaa02d5216a649df
huoyan298 <YTestEamil@163.com>
2021-10-18 10:33:12 +0800

    exercise

--- 68geneizhi.py
+++ 68geneizhi.py
@@ -132,11 +132,124 @@ b=['x','y','z']
 c=a+b
 
 
-l=itertools.chain(*c)
-# 这里要思考下，遍历后，list(l)打印为[]
+# l=itertools.chain(*c)
+# # 这里要思考下，遍历后，list(l)打印为[]
 # for i in l:
 #     print(c)
+# print(type(l))
+# print(list(l))
 
-print(type(l))
-print(list(l))
 
+l=[]
+a={}
+for i in range(0,7):
+    print("l", id(l))
+    a[i] = i
+    print(a[0])
+    print(a[i])
+    print(a)
+    l.append(a)
+    print( "id(a),id(a[i])", id(a),id(a[i]) )
+    # l.append(a)
+    # print(l)
+    # print(id(l))
+
+
+# l=[]
+# a={'num':0}
+# for i in range(10):
+#     a['num']=i
+#     print( "a:",a )
+#     l.append(a)
+#     print( l )
+
+
+# dict={"1":8,"1":9}
+# print("dict",dict)
+# dict["1"]=0
+# print(dict)
+
+
+# [{1: 1, 2: 2, 3: 3, 4: 4}, {1: 1, 2: 2, 3: 3, 4: 4}, {1: 1, 2: 2, 3: 3, 4: 4}, {1: 1, 2: 2, 3: 3, 4: 4}]
+# l=[]
+# a={1: 1}
+# b={1: 1, 2: 2}
+# c={1: 1, 2: 2, 3: 3}
+# print("a,b,c",a,b,c)
+# l.append(a)
+# print("l1:",l)
+# l.append(b)
+# print("l2:",l)
+# l.append(c)
+# print("l3:",l)
+
+# 9*9乘法表
+'''
+for i in range(1,10):
+    for j in range(1,i+1):
+        print("{}x{}={}".format(i,j,i*j),end="\t")
+    print()
+'''
+#
+'''
+def test():
+     he="hello world ni hao congratunation"
+     word="ni"
+     # if word in he:
+     for i in he:
+         return he.find(word)
+     else:
+         return -1
+
+print(test())
+'''
+#判断是不是纯数字
+# def  test_to_num(num):
+#      try:
+#         return int(num)
+#      except  ValueError:
+#          return "qingshuru"
+#
+# print(test_to_num(123))
+
+# 打印星形图
+# def test_xing():
+#     n=7
+#     for i in range(-int(n/2),int(n/2)+1):
+#         print(" "*abs(i),"*"*abs(n-abs(i)*2))
+#
+# test_xing()
+
+'''
+# 水仙花数
+def test_shuixianhua():
+    for num in range(100,1000):
+        i = num//100
+        j=num//10%10
+        k = num % 10
+        if i**3 +j**3 +k**3 ==num:
+            print(str(num))
+
+test_shuixianhua()
+'''
+'''
+# 1-2+3-4+5-6+...-100
+def test_sum(num_end):
+    sum_all=0
+    for i in range(1,num_end+1):
+        sum_all+=i*(-1)**(i+1)
+    return sum_all
+
+print(test_sum(100))
+'''
+# sum+=i*10+i
+def test_sum_h(n):
+    sum=0
+    for i in range(1,n+1):
+        t=i*10+3
+        print(t)
+        sum+=t
+    return sum
+print(test_sum_h(5))
+
+# 对称数组 if x==x[::-1]
